15888183ce7d035d2be7f2d2b77b3677aee72f3d
George Spasov <spasov05203@gmail.com>
2019-08-06 11:04:17 +0300

    Fixed minor UI Glitch

--- src/components/Simulator.vue
+++ src/components/Simulator.vue
@@ -116,9 +116,6 @@
           </div>
         </div>
         <div class="col-12">
-          <hr />
-        </div>
-        <div class="col-12">
           <div class="row">
             <div class="col-4 right-separator">
               <h4>Invest</h4>
